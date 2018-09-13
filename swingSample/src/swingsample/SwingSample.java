e35106206a6d0f226a61c2394a568f849b904b6a
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-09-13 09:26:59 +0545

    swing form called from main

--- swingSample/src/swingsample/SwingSample.java
+++ swingSample/src/swingsample/SwingSample.java
@@ -5,18 +5,53 @@
  */
 package swingsample;
 
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
 /**
  *
  * @author yubaraj
  */
 public class SwingSample {
+JDesktopPane desk;
+	JInternalFrame frame1, frame2, frame3, frame4;
+	JFrame frame;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Calculator calculator = new Calculator();
+//        Calculator calculator = new Calculator();
+//        ButtonSample sample = new ButtonSample();
+//        SwingSample d = new SwingSample();
+//          Quiz quiz = new Quiz();
+        JAVASwingFormExample example = new JAVASwingFormExample();
     }
     
+     public SwingSample() {
+		frame = new JFrame("Multiple Frames");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		desk = new JDesktopPane();
+		frame1 = new JInternalFrame("Frame1", true, true, true, true);
+		frame1.setBounds(20, 200, 150, 100);
+		frame1.setVisible(true);
+		frame2 = new JInternalFrame("Frame2", true, true, true, true);
+		frame2.setBounds(20, 140, 150, 100);
+		frame2.setVisible(true);
+		frame3 = new JInternalFrame("Frame3", true, true, true, true);
+		frame3.setBounds(20, 80, 150, 100);
+		frame3.setVisible(true);
+		frame4 = new JInternalFrame("Frame4", true, true, true, true);
+		frame4.setBounds(20, 20, 150, 100);
+		frame4.setVisible(true);
+		desk.add(frame1);
+		desk.add(frame2);
+		desk.add(frame3);
+		desk.add(frame4);
+		frame.add(desk);
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+	}
 }
